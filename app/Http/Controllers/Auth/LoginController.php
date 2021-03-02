e6f6365ce354a93bced20fcc7fbd10283009344a
Onee-Chan <gilangainun69.haltec@gmail.com>
2021-03-02 19:32:24 +0700

    first commit

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+
+
+class LoginController extends Controller
+{
+   use AuthenticatesUsers;
+
+
+   protected $redirectTo = '/home';
+   protected function redirectTo()
+   {
+       if (auth()->user()->role == 'admin') {
+           return '/admin';
+       }
+       return '/home';
+   }
+
+
+    public function __construct()
+   {
+       $this->middleware('guest')->except('logout');
+   }
+}
